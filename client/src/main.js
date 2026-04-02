756ac966f022d8ff72a5c4ec5e7f97c5188c20da
Max <louamax@gmail.com>
2026-04-02 19:00:44 +0200

    fix: use CSS font-based MDI icons instead of SVG iconset

--- client/src/main.js
+++ client/src/main.js
@@ -4,13 +4,9 @@ import router from './router';
 import store from './store/store';
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
-import { aliases, mdi } from 'vuetify/iconsets/mdi';
-
 const vuetify = createVuetify({
     icons: {
         defaultSet: 'mdi',
-        aliases,
-        sets: { mdi },
     },
     theme: {
         defaultTheme: 'dark',
